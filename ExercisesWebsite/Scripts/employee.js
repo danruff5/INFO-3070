b7b5c3c1eea2eb576314c786c36633372c7ca8eb
Daniel Krauskopf <dckrauskopf@gmail.com>
2015-09-24 19:23:19 -0400

    Finished Lab 4

--- ExercisesWebsite/Scripts/employee.js
+++ ExercisesWebsite/Scripts/employee.js
@@ -1,6 +1,7 @@
 ﻿$(function () {
     $("#empbutton").click(function (e) {
         var lastname = $("#TextBoxLastname").val();
+        $("#lblstatus").text("Please be patient...");
         ajaxCall("Get", "api/employees/" + lastname, "")
         .done(function (data) {
             if (data.Lastname !== "not found") {
@@ -8,11 +9,24 @@
                 $("#title").text(data.Title);
                 $("#firstname").text(data.Firstname);
                 $("#phone").text(data.Phoneno);
+
+                ajaxCall("Get", "api/departments/" + data.DepartmentId, "")
+                .done(function (data) {
+                    if (data.DepartmentId !== "not found") {
+                        $("#departmentname").text(data.DepartmentName);
+                    } else {
+                        $("#departmentname").text("Department not found");
+                    }
+                }).fail(function (jqXHR, textStatus, errorThrown) {
+                    errorRoutine(jqXHR);
+                });
+                $("#lblstatus").text("Employee " + data.Lastname + " Found!");
             } else {
-                $("#firstname").text("Not Found");
+                $("#firstname").text("");
                 $("#email").text("");
                 $("#title").text("");
                 $("#phone").text("");
+                $("#lblstatus").text("Employee "+ data.Lastname + " not found");
             }
         })
         .fail(function (jqXHR, textStatus, errorThrown) {
@@ -22,7 +36,7 @@
 });
 
 function ajaxCall(type, url, data) {
-    return $.ajax({ // Returnt he promise that $.ajax returns
+    return $.ajax({ // Return the promise that $.ajax returns
         type: type,
         url, url,
         data: JSON.stringify(data),
