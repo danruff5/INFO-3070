efb42f2c940c1f489e9963537494c73fa6aed7bc
Daniel Krauskopf <dckrauskopf@gmail.com>
2015-09-26 12:48:34 -0400

    Fixed Lab 4

---
+++ ExercisesWebsite/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Http;
+using System;
+using ExcerciseViewModels;
+
+namespace ExercisesWebsite
+{
+    public class EmployeeController : ApiController
+    {
+        [Route("api/employees/{name}")]
+        public IHttpActionResult Get(string name)
+        {
+            try
+            {
+                EmployeeViewModel emp = new EmployeeViewModel();
+                emp.Lastname = name;
+                emp.GetBySurname();
+                return Ok(emp);
+            } catch (Exception ex)
+            {
+                return BadRequest("Retrive failed - " + ex.Message);
+            }
+        }
+    }
+}
