549cd7c3b97bd7a1292824917cf00cee73ed2985
Daniel Krauskopf <dckrauskopf@gmail.com>
2015-09-26 13:03:11 -0400

    Added C# workign files

---
+++ ExcercisesDAL/DepartmentDAO.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Driver.Linq;
+using System.Linq;
+using System;
+using MongoDB.Bson;
+
+
+namespace ExcercisesDAL
+{
+    public class DepartmentDAO
+    {
+        public Department GetDepartment (string idS)
+        {
+            ObjectId id = new ObjectId(idS);
+            Department retDep = null;
+            DbContext _ctx;
+
+            try
+            {
+                _ctx = new DbContext();
+                var departments = _ctx.Departments;
+                var department = departments.AsQueryable<Department>().Where(dep => dep._id == id).FirstOrDefault();
+                retDep = (Department)department;
+            } catch (Exception ex)
+            {
+                Console.WriteLine("Problem " + ex.Message);
+            }
+            return retDep;
+        }
+    }
+}
